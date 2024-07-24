0a6c6096f488664639dc89f4eae95cb162c8e639
DSpro1 <danielverissimo1d@gmail.com>
2024-07-23 21:56:14 -0300

    verificação do prazo de validade do contrato, para identificar se o prazo de validade continua ou não válido

--- biddingContract/biddingContracts/views.py
+++ biddingContract/biddingContracts/views.py
@@ -3,66 +3,10 @@ from django.http import HttpResponseRedirect
 from  .forms import formLicitacao, formFornecedor, formContrato
 from django.urls import reverse
 from .models import Licitacao, Fornecedor, Contrato, NotaFiscal
+import datetime
+from datetime import date
 # Create your views here.
 
-
-# LICITACÃO
-def cadLicitacao(request):
-    if (request.method == 'POST'):
-        form = formLicitacao(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('listLicitacoes'))
-    else:
-        form = formLicitacao()
-    return render(request, 'biddingContracts/licitacao_new.html', {"form": form})
-
-def listLicitacoes(request):
-    """mostra todas as licitacoes"""
-    licitacoes = Licitacao.objects.all()
-    context = {"licitacoes": licitacoes}
-   
-    return render(request, "biddingContracts/licitacoes.html", context)
-   
-
-# FORNECEDOR
-def cadFornecedor(request):
-    if request.method == 'POST':
-        form = formFornecedor(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('fornecedores'))
-    else:
-        form = formFornecedor()
-
-    return render(request, 'biddingContracts/fornecedor_new.html', {"form": form})
-
-def listFornecedores(request):
-    fornecedores = Fornecedor.objects.all()
-    context = {"fornecedores": fornecedores}
-    return render(request, "biddingContracts/fornecedores.html", context)
-"""
-def infoFornecedores(request, forn_id):
-  
-    fornecedor = Fornecedor.objects.get(id = forn_id)
-    
-    context = {"fornecedor": fornecedor}
-
-    return render(request, "biddingContracts/fornecedor.html", context)"""
-
-
-# CONTRATO
-def cadContrato(request):
-    if request.method == "POST":
-        form = formContrato(request.POST),
-
-        if form.is_valid:
-            form.save()
-            return HttpResponseRedirect(reverse("contratos"))
-    else:
-        form = formContrato()
-    return render(request, "biddingContracts/contrato_new.html", {"form": form})
 
 def listContratos(request):
     contratos = Contrato.objects.all()
@@ -72,17 +16,38 @@ def listContratos(request):
 def contratosRelatorio(request, id_contrato):
     contrato = Contrato.objects.get(numero=id_contrato)
     notasFiscais = NotaFiscal.objects.filter(contrato_fk = id_contrato)
+    
     saldoAtual = contrato.valor
-    vigencia = contrato.dataFinal - contrato.dataInicial
+    
+    hoje = date.today()
+    dataFinalContrato = contrato.dataFinal
+
+    prazoRestante=verifica_prazo_validade_contrato(dataFinalContrato, hoje)
+
     for notas in notasFiscais:
-        if notas.contrato_fk.numero ==contrato.numero:
+        if notas.contrato_fk.numero == contrato.numero:
             saldoAtual -= notas.valor
     context = {
         "notasfiscais": notasFiscais,
         "saldoAtual": saldoAtual,
-        "vigencia": vigencia
+        "vigencia": prazoRestante
         }
     return render(request, "contratos_relatorio.html", context)
+
+def verifica_prazo_validade_contrato(dataFinal, hoje):
+    anoFinalContrato = dataFinal.year
+    mesFinalContrato = dataFinal.month
+    diaFinalContrato = dataFinal.day
+    anoAtual = hoje.year
+    mesAtual = hoje.month
+    diaAtual = hoje.day
+    dataFinalContrato_dateTime = datetime.datetime(anoFinalContrato, mesFinalContrato, diaFinalContrato)
+    dataAtual_dateTime = datetime.datetime(anoAtual, mesAtual, diaAtual) 
+
+    if dataFinalContrato_dateTime >= dataAtual_dateTime:
+        return dataFinalContrato_dateTime - dataAtual_dateTime
+
+    return f"contrato passou do prazo: {dataFinalContrato_dateTime.strftime("%d/%m/%Y")}"
 
 # INDEX
 def index(request):
