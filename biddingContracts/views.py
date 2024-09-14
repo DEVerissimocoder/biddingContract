e71d7cfab82521577798336d67c1bdbaf2881a07
DSpro1 <danielverissimo1d@gmail.com>
2024-09-14 20:39:21 -0300

    alterações de id do banco de dados

--- biddingContracts/views.py
+++ biddingContracts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views import View
 from django.views.generic import UpdateView
