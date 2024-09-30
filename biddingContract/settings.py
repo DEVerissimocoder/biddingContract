296ca157eb81870437007b4312149da60962e8fd
devmarcs <devmarcelo.gus@gmail.com>
2024-09-29 21:07:10 -0300

    Modificando o settings

--- biddingContract/settings.py
+++ biddingContract/settings.py
@@ -205,13 +205,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuração de email
 #EMAIL_BACKEND = config("EMAIL_BACKEND")
-DEFAULT_FROM_EMAIL= "devmarcelo.gus@gmail.com"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "devmarcelo.gus@gmail.com"
-EMAIL_HOST_PASSWORD = "rgeqozmjdosngzis"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 #Messages Flash
 
