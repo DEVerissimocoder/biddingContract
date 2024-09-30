3daa5f71d6fb933f07ded722ad91f09c0a46dfe0
devmarcs <devmarcelo.gus@gmail.com>
2024-09-30 14:00:43 -0300

    Ajustando o db_host para funcionar no sistema

--- biddingContract/settings.py
+++ biddingContract/settings.py
@@ -107,13 +107,24 @@ WSGI_APPLICATION = 'biddingContract.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.getenv('DB_HOST'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('DB_HOST').split('/')[0],
         'PORT': os.getenv('DB_PORT'),
     }
 }
