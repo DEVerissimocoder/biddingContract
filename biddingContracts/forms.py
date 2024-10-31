475cb9c873c699ebb2092b3eec0330d1071c0856
DSpro1 <danielverissimo1d@gmail.com>
2024-10-31 16:31:29 -0300

    tela de cadastro de contrato campo fornecedor_fk - aparecer o nome do fornecedor em vez do id, pronto para teste

--- biddingContracts/forms.py
+++ biddingContracts/forms.py
@@ -191,11 +191,11 @@ class formContrato(forms.ModelForm):
         }
         widgets = {
           
-            "fornecedor_fk": forms.TextInput(
+            "fornecedor_fk": forms.HiddenInput(
                 attrs={
-                    "type": "text",
+                    "type": "hidden",
                     "class": "form-control",
-                    "placeholder": "fornecedor",
+                    
                 }
             ),
 
