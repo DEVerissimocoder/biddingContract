263a9c3caa9391cfdb2240f35ad5626e0a95510e
DSpro1 <danielverissimo1d@gmail.com>
2024-11-02 10:28:41 -0300

    alteração no campo fornecedor da tela de alteração da tela de atualização de arp

--- biddingContracts/views.py
+++ biddingContracts/views.py
@@ -651,6 +651,9 @@ class ARPsUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        contratoParaAlterar = self.get_object()
+        forn = contratoParaAlterar.fornecedor_fk
+        context['forn'] = forn
         return context
 
 
