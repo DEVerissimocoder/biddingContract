aa7a188720e151e595d1ac0829c336fec46cd294
DSpro1 <danielverissimo1d@gmail.com>
2024-12-09 21:38:56 -0300

    desfazendo commit anterior

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-#User = get_user_model()
+User = get_user_model()
 
 
-user=User.objects.get(username='root')
-
-user.set_password('137')
-user.save()
-print("usuario salvo")
 
 
 
